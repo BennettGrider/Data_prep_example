88040dfb13d33f8dfbdcf643ce950b6531e70568
Bennett Grider <42590342+BennettGrider@users.noreply.github.com>
2018-10-17 20:25:44 -0500

    Created a 'create empty shapefile' function

--- dataprep.py
+++ dataprep.py
@@ -1,5 +1,7 @@
 import numpy as np
 from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
 import rasterio
 import fiona
 
@@ -10,7 +12,7 @@ import pprint
 import pytest
 
 
-'''
+"""
 STEPS:
 Open survey area shapefile
 Open basedata raster datasets
@@ -20,7 +22,8 @@ Clip raster datasets to buffered survey area
 Possibly reclassify a raster dataset, vectorize?
 Clip vector datasets to buffered survey area (keep the entirety of a road feature 
  up to 1[or user input] miles away)
-'''
+"""
+
 
 
 def user_select_file(file_type):
@@ -36,7 +39,8 @@ def user_select_file(file_type):
 
 
 def open_vector_dataset(feature_list, file):
-    """Uses fiona to open a shapefile dataset, appending all vector features to the "features" list input"""
+    """Uses fiona to open a shapefile dataset, appending all vector features to the "features" list input.
+    feature_list is a list that will store the shapefile information, and file is the file directory."""
 
     # Making sure the inputs are valid
     if type(feature_list) is not list:
@@ -54,6 +58,69 @@ def open_vector_dataset(feature_list, file):
     return feature_list
 
 
+def open_raster_dataset(file, band_number=1):
+    """Uses rasterio to open a raster dataset, where file is the directory of the raster file and band_number is
+    the desired band to be read (defaults to the first band)."""
+    # TODO: finish writing this!
+    with rasterio.open(file) as dataset:
+        print(f'{dataset.name}, {dataset.mode}, {dataset.width}, {dataset.height}, {dataset.crs}')
+        print(f'Opening raster file at {file}')
+        extracted_crs = dataset.crs.to_dict()['init']
+        print(f'Shape: {dataset.shape}, CRS: {extracted_crs}, Bands: {dataset.indexes}')
+
+        array = dataset.read(band_number)
+
+    return array
+
+
+# Not strictly necessary for this example, but used to create the test survey dataset and for future use
+def create_new_shapefile(fields, epsg_crs_code, filename = 'New_Shapefile', feature_type = 'point'):
+    """Provided a file directory/name, a tuple of field attributes in the format (type, name), and optionally a list(?)
+    of features to add to the shapefile, creates a brand new shapefile."""
+    driver = ogr.GetDriverByName("ESRI Shapefile") # Set up driver
+    data_source = driver.CreateDataSource(filename + '.shp') # Create data source
+    srs = osr.SpatialReference() # Create spatial reference
+    srs.ImportFromEPSG(epsg_crs_code) # The epsg code must be the number portion of the code only!
+    if feature_type.lower() == 'point':
+        feat_type = ogr.wkbPoint
+    elif feature_type.lower() == 'line':
+        feat_type = ogr.wkbLineString
+    elif feature_type.lower() == 'polygon':
+        feat_type = ogr.wkbPolygon
+    layer = data_source.CreateLayer(filename, srs, feat_type) # Creating layer, defaults to point type
+
+    # Adding fields
+    for field in fields:
+        if field[0] == 'field_int':
+            layer.CreateField(ogr.FieldDefn(field[1], ogr.OFTInteger))
+        elif field[0] == 'field_float':
+            layer.CreateField(ogr.FieldDefn(field[1], ogr.OFTReal))
+        elif field[0] == 'field_str':
+            field_name = ogr.FieldDefn(field[1], ogr.OFTString)
+            field_name.SetWidth(24)
+            layer.CreateField(field_name)
+
+    # For checking to make sure that the attributes actually got properly added to the layer
+    schema = []
+    # ldef = layer.GetLayerDefn()
+    # for n in range(ldef.GetFieldCount()):
+    #     fdef = ldef.GetFieldDefn(n)
+    #     schema.append(fdef.name)
+    # print(schema)
+
+    data_source = None # Saving and closing the data source
+
+
+
+
+
+def smart_clip_roads(clip_vector, roads, buffer = 0.5, max_road_extension = 1):
+    """Takes two data inputs, a list of vector data (survey data or anything else) and a roads vector data list,
+    and creates a smart buffer around the survey data. The roads vector data is clipped to a buffer of the survey
+    data, but road features are preserved if they connect to a different road within a maximum extension distance."""
+    # TODO: write all this as well!
+
+
 
 
 
@@ -67,8 +134,21 @@ if __name__ == '__main__':
 
 
     # Opening the vector basedata
-    roads = [] # List to contain all vector features from the survey area shapefile
-    open_vector_dataset(roads, user_select_file("vector")) # 'data/tl_2015_27_prisecroads.shp'
+    #roads = [] # List to contain all vector features from the survey area shapefile
+    #open_vector_dataset(roads, user_select_file("vector")) # 'data/tl_2015_27_prisecroads.shp'
+
+    testRast = open_raster_dataset('data/n44_w094_1arc_v3.tif')
+
+    shapefile = "data/tl_2015_27_prisecroads.shp"
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    dataSource = driver.Open(shapefile, 0) # 0 specifies that data can't be written to, a 1 would mean it can be written to
+    layer = dataSource.GetLayer()
+
+    testFields = [('field_float', 'Latitude'), ('field_float', 'Longitude'),
+                  ('field_int', 'Number'), ('field_str', 'Notes')]
+    create_new_shapefile(testFields, 4326, filename='data/test_empty_shapefile', feature_type='line')
+
+
 
 
 
